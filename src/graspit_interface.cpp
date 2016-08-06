f429619586ad252cef59949a11ebe8f37b018cfb
Jack Rasiel <jrasiel@haverford.edu>
2016-08-06 00:19:57 -0400

    Added body name field to relevant msgs and srvs.

--- src/graspit_interface.cpp
+++ src/graspit_interface.cpp
@@ -126,11 +126,30 @@ bool GraspitInterface::getRobotCB(graspit_interface::GetRobot::Request &request,
     return true;
 }
 
-bool GraspitInterface::getGraspableBodyCB(graspit_interface::GetGraspableBody::Request &request,
-                graspit_interface::GetGraspableBody::Response &response)
-{
-    if (graspitCore->getWorld()->getNumGB() <= request.id) {
-        response.result = response.RESULT_INVALID_ID;
+    bool GraspitInterface::getGraspableBodyCB(graspit_interface::GetGraspableBody::Request &request,
+            graspit_interface::GetGraspableBody::Response &response)
+    {
+        if (graspitCore->getWorld()->getNumGB() <= request.id) {
+            response.result = response.RESULT_INVALID_ID;
+            return true;
+        } else {
+            GraspableBody *b = graspitCore->getWorld()->getGB(request.id);
+            transf t = b->getTran();
+
+            geometry_msgs::Pose pose = geometry_msgs::Pose();
+
+            pose.position.x = t.translation().x() / 1000.0;
+            pose.position.y = t.translation().y() / 1000.0;;
+            pose.position.z = t.translation().z() / 1000.0;;
+            pose.orientation.w = t.rotation().w;
+            pose.orientation.x = t.rotation().x;
+            pose.orientation.y = t.rotation().y;
+            pose.orientation.z = t.rotation().z;
+
+            response.graspable_body.pose = pose;
+            response.graspable_body.element_name = b->getName().toStdString();
+            return true;
+        }
         return true;
     } else {
         GraspableBody *b = graspitCore->getWorld()->getGB(request.id);
@@ -146,17 +165,41 @@ bool GraspitInterface::getGraspableBodyCB(graspit_interface::GetGraspableBody::R
         pose.orientation.y = t.rotation().y;
         pose.orientation.z = t.rotation().z;
 
-        response.graspable_body.pose = pose;
+    bool GraspitInterface::getBodyCB(graspit_interface::GetBody::Request &request,
+            graspit_interface::GetBody::Response &response)
+    {
+        if (graspitCore->getWorld()->getNumBodies() <= request.id) {
+            response.result = response.RESULT_INVALID_ID;
+            return true;
+        } else {
+            Body *b = graspitCore->getWorld()->getBody(request.id);
+            transf t = b->getTran();
+
+            geometry_msgs::Pose pose = geometry_msgs::Pose();
+
+            pose.position.x = t.translation().x() / 1000.0;
+            pose.position.y = t.translation().y() / 1000.0;;
+            pose.position.z = t.translation().z() / 1000.0;;
+            pose.orientation.w = t.rotation().w;
+            pose.orientation.x = t.rotation().x;
+            pose.orientation.y = t.rotation().y;
+            pose.orientation.z = t.rotation().z;
+
+            response.body.pose = pose;
+            response.body.element_name = b->getName().toStdString(); 
+
+            return true;
+        }
         return true;
     }
-    return true;
-}
 
-bool GraspitInterface::getBodyCB(graspit_interface::GetBody::Request &request,
-                graspit_interface::GetBody::Response &response)
-{
-    if (graspitCore->getWorld()->getNumBodies() <= request.id) {
-        response.result = response.RESULT_INVALID_ID;
+    bool GraspitInterface::getRobotsCB(graspit_interface::GetRobots::Request &request,
+            graspit_interface::GetRobots::Response &response)
+    {
+        for (int i=0; i < graspitCore->getWorld()->getNumRobots(); i++)
+        {
+            response.ids.push_back(i);
+        }
         return true;
     } else {
         Body *b = graspitCore->getWorld()->getBody(request.id);
@@ -174,63 +217,74 @@ bool GraspitInterface::getBodyCB(graspit_interface::GetBody::Request &request,
 
         response.body.pose = pose;
 
+    bool GraspitInterface::getGraspableBodiesCB(graspit_interface::GetGraspableBodies::Request &request,
+            graspit_interface::GetGraspableBodies::Response &response)
+    {
+        for (int i=0; i < graspitCore->getWorld()->getNumGB(); i++)
+        {
+            response.ids.push_back(i);
+        }
         return true;
     }
-    return true;
-}
 
-bool GraspitInterface::getRobotsCB(graspit_interface::GetRobots::Request &request,
-                graspit_interface::GetRobots::Response &response)
-{
-    for (int i=0; i < graspitCore->getWorld()->getNumRobots(); i++)
+    bool GraspitInterface::getBodiesCB(graspit_interface::GetBodies::Request &request,
+            graspit_interface::GetBodies::Response &response)
     {
-        response.ids.push_back(i);
-    }
-    return true;
-}
-
-bool GraspitInterface::getGraspableBodiesCB(graspit_interface::GetGraspableBodies::Request &request,
-                graspit_interface::GetGraspableBodies::Response &response)
-{
-    for (int i=0; i < graspitCore->getWorld()->getNumGB(); i++)
-    {
-        response.ids.push_back(i);
-    }
-    return true;
-}
-
-bool GraspitInterface::getBodiesCB(graspit_interface::GetBodies::Request &request,
-                graspit_interface::GetBodies::Response &response)
-{
-    for (int i=0; i < graspitCore->getWorld()->getNumBodies(); i++)
-    {
-        response.ids.push_back(i);
-    }
-    return true;
-}
-
-bool GraspitInterface::setRobotPoseCB(graspit_interface::SetRobotPose::Request &request,
-                graspit_interface::SetRobotPose::Response &response)
-{
-    if (graspitCore->getWorld()->getNumRobots() <= request.id) {
-        response.result = response.RESULT_INVALID_ID;
-        return true;
-    } else {
-        vec3 newTranslation(request.pose.position.x * 1000.0,
-                            request.pose.position.y * 1000.0,
-                            request.pose.position.z * 1000.0);
-
-        Quaternion newRotation(request.pose.orientation.w,
-                               request.pose.orientation.x,
-                               request.pose.orientation.y,
-                               request.pose.orientation.z);
-
-        transf newTransform(newRotation, newTranslation);
-
-        graspitCore->getWorld()->getRobot(request.id)->setTran(newTransform);
+        for (int i=0; i < graspitCore->getWorld()->getNumBodies(); i++)
+        {
+            response.ids.push_back(i);
+            QString name = graspitCore->getWorld()->getBody(i)->getName();
+            response.element_names.push_back(name.toStdString());
+        }
         return true;
     }
-}
+
+    bool GraspitInterface::setRobotPoseCB(graspit_interface::SetRobotPose::Request &request,
+            graspit_interface::SetRobotPose::Response &response)
+    {
+        if (graspitCore->getWorld()->getNumRobots() <= request.id) {
+            response.result = response.RESULT_INVALID_ID;
+            return true;
+        } else {
+            vec3 newTranslation(request.pose.position.x * 1000.0,
+                    request.pose.position.y * 1000.0,
+                    request.pose.position.z * 1000.0);
+
+            Quaternion newRotation(request.pose.orientation.w,
+                    request.pose.orientation.x,
+                    request.pose.orientation.y,
+                    request.pose.orientation.z);
+
+            transf newTransform(newRotation, newTranslation);
+
+            graspitCore->getWorld()->getRobot(request.id)->setTran(newTransform);
+            return true;
+        }
+    }
+
+    bool GraspitInterface::setGraspableBodyPoseCB(graspit_interface::SetGraspableBodyPose::Request &request,
+            graspit_interface::SetGraspableBodyPose::Response &response)
+    {
+        if (graspitCore->getWorld()->getNumGB() <= request.id) {
+            response.result = response.RESULT_INVALID_ID;
+            return true;
+        } else {
+
+            vec3 newTranslation(request.pose.position.x * 1000.0,
+                    request.pose.position.y * 1000.0,
+                    request.pose.position.z * 1000.0);
+
+            Quaternion newRotation(request.pose.orientation.w,
+                    request.pose.orientation.x,
+                    request.pose.orientation.y,
+                    request.pose.orientation.z);
+
+            transf newTransform(newRotation, newTranslation);
+
+            graspitCore->getWorld()->getGB(request.id)->setTran(newTransform);
+            return true;
+        }
+    }
 
 bool GraspitInterface::setGraspableBodyPoseCB(graspit_interface::SetGraspableBodyPose::Request &request,
                 graspit_interface::SetGraspableBodyPose::Response &response)
