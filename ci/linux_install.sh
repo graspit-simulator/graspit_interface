f6feb5e0d23db710e6cf6238087385c6590b2253
iretiayo <iretibob@gmail.com>
2017-07-31 16:31:11 -0400

    adding travis test

---
+++ ci/linux_install.sh
@@ -0,0 +1,22 @@
+APT='
+cmake
+libqt4-dev
+libqt4-opengl-dev
+libqt4-sql-psql
+libcoin80-dev
+libsoqt4-dev
+libblas-dev
+liblapack-dev
+libqhull-dev 
+libeigen3-dev
+python-catkin-pkg
+python-rosdep
+python-wstool
+ros-$ROS_DISTRO-catkin
+ros-$ROS_DISTRO-sensor-msgs
+ros-$ROS_DISTRO-geometry-msgs
+ros-$ROS_DISTRO-tf-conversions
+ros-$ROS_DISTRO-shape-msgs
+'
+
+sudo apt-get -qq --yes --force-yes install $APT
