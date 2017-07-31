d4fc3ea27e3a44b771654331aaef29bca2373390
iretiayo <iretibob@gmail.com>
2017-07-31 16:50:01 -0400

    fixing ros_distro

--- ci/install.sh
+++ ci/install.sh
@@ -14,11 +14,11 @@ libeigen3-dev
 python-catkin-pkg
 python-rosdep
 python-wstool
-ros-$ROS_DISTRO-catkin
-ros-$ROS_DISTRO-sensor-msgs
-ros-$ROS_DISTRO-geometry-msgs
-ros-$ROS_DISTRO-tf-conversions
-ros-$ROS_DISTRO-shape-msgs
+ros-${ROS_DISTRO}-catkin
+ros-${ROS_DISTRO}-sensor-msgs
+ros-${ROS_DISTRO}-geometry-msgs
+ros-${ROS_DISTRO}-tf-conversions
+ros-${ROS_DISTRO}-shape-msgs
 '
 
 sudo apt-get -qq --yes --force-yes install $APT
